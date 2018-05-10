ba0ac2d9986d08c12533a069e91b3bc1543b9959
PePo <gmgame.1525@gmail.com>
2018-05-09 19:36:20 -0700

    Complate mission 4
    - resize sprite asset for mission 5

--- Assets/Scripts/Mission/Giraff.cs
+++ Assets/Scripts/Mission/Giraff.cs
@@ -1,16 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.UI;
 public class Giraff : MonoBehaviour {
+	public GameObject leg, tail;
+	public GameObject legHint, tailHint;
+	public AudioClip AudioWrong, AudioCorrect;
 
-	// Use this for initialization
-	void Start () {
-		
+	private bool legClick = false;
+	private bool tailClick = false;
+
+	public void InputClickTail() {
+		if (!tailClick) {
+			AudioSource.PlayClipAtPoint(AudioCorrect, new Vector3(0f, 0f, -10f));
+			tail.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+			tailClick = true;
+			tailHint.SetActive(false);
+		}
+		WinCondition();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void InputClickLeg() {
+		if (!legClick) {
+			AudioSource.PlayClipAtPoint(AudioCorrect, new Vector3(0f, 0f, -10f));
+			leg.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+			legClick = true;
+			legHint.SetActive(false);
+		}
+		WinCondition();
+	}
+	public void WrongClick() {
+		AudioSource.PlayClipAtPoint(AudioWrong, new Vector3(0f, 0f, -10f));
+		if (!legClick) {
+			legHint.SetActive(true);
+		} else {
+			tailHint.SetActive(true);
+		}
+	}
+	public void FinishStory() {
+
+	}
+
+	private void WinCondition() {
+		if (legClick && tailClick) {
+			gameObject.SendMessage("Win");
+		}
 	}
 }
