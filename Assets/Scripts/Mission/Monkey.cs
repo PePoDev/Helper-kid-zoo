fb076c227a8799030d10fb30413c01fbce440636
PePo <gmgame.1525@gmail.com>
2018-05-09 21:38:22 -0700

    Complate mission 5

--- Assets/Scripts/Mission/Monkey.cs
+++ Assets/Scripts/Mission/Monkey.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Monkey : MonoBehaviour {
+	public GameObject mainPanel, playPanel;
+	public GameObject MonkeyObj;
+	public Sprite MonkeyEatBanana;
+	public GameObject[] InputStep;
+	public GameObject[] HintStep;
+	public GameObject[] AnswerStep;
+	public GameObject WinObj;
+	public AudioClip WinAudio, CorrectAudio, WrongAudio;
 
-	// Use this for initialization
-	void Start () {
-		
+	private int currentStep = 0;
+
+	public void ClickInput(int i) {
+		if (currentStep == i) {
+			AudioSource.PlayClipAtPoint(CorrectAudio, new Vector3(0f, 0f, -10f));
+			HintStep[currentStep].SetActive(false);
+			InputStep[i].SetActive(false);
+			AnswerStep[i].SetActive(true);
+			currentStep++;
+		} else {
+			AudioSource.PlayClipAtPoint(WrongAudio, new Vector3(0f, 0f, -10f));
+			HintStep[currentStep].SetActive(true);
+		}
+		if (currentStep == 3) {
+			StartCoroutine(WinMonkey());
+			currentStep++;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void FinishStory() {
+		StartCoroutine(WaitToDisplayPlayPanel());
+	}
+	IEnumerator WaitToDisplayPlayPanel() {
+		yield return new WaitForSeconds(1.5f);
+		mainPanel.SetActive(true);
+		playPanel.SetActive(true);
+	}
+	IEnumerator WinMonkey() {
+		WinObj.SetActive(true);
+		AudioSource.PlayClipAtPoint(WinAudio, new Vector3(0f, 0f, -10f));
+		yield return new WaitForSeconds(1.5f);
+		mainPanel.SetActive(false);
+		playPanel.SetActive(false);
+		gameObject.SendMessage("Win");
 	}
 }
