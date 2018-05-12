1b86ae52b50ebf71c5cee822c531c483c6fcb511
PePo <gmgame.1525@gmail.com>
2018-05-13 00:06:10 +0700

    Complate mission 8

--- Assets/Scripts/Mission/Parrot.cs
+++ Assets/Scripts/Mission/Parrot.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Parrot : MonoBehaviour {
+	public GameObject[] CorrectBridObj;
+	public GameObject[] InputBridObj;
+	public GameObject[] HintBridObj;
+	public GameObject[] BridStepCorrect;
+	public AudioClip Correct, Worng;
 
-	// Use this for initialization
-	void Start () {
-		
+	private int currentAnswer = 0;
+	private bool canClick = false;
+
+	public void InputClick(int i) {
+		if (canClick) {
+			if (currentAnswer == i) {
+				AudioSource.PlayClipAtPoint(Correct, gameObject.transform.position);
+				CorrectBridObj[i].SetActive(true);
+				InputBridObj[i].SetActive(false);
+				HintBridObj[i].SetActive(false);
+				currentAnswer++;
+				if (currentAnswer == 3) {
+					gameObject.SendMessage("ClearMission",7);
+				}
+			} else {
+				AudioSource.PlayClipAtPoint(Worng, gameObject.transform.position);
+				HintBridObj[currentAnswer].SetActive(true);
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void FinishStory() {
+		StartCoroutine(Question());
+	}
+	IEnumerator Question() {
+		for (int i = 0; i < BridStepCorrect.Length; i++) {
+			BridStepCorrect[i].SetActive(true);
+			yield return new WaitForSeconds(1f);
+			BridStepCorrect[i].SetActive(false);
+		}
+		canClick = true;
 	}
 }
