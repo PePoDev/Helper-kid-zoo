fd039c8285374fee1ce245c76da02516707a3851
PePo <gmgame.1525@gmail.com>
2018-05-14 04:48:00 +0700

    Fix Elephant and Add apps icon

--- Assets/Scripts/Mission/Elephant.cs
+++ Assets/Scripts/Mission/Elephant.cs
@@ -10,26 +10,32 @@ public class Elephant : MonoBehaviour {
 	public Sprite[] AnimalSprite;
 	public AudioClip[] AnimalVoice;
 	public AudioClip QuestionVoice;
+	public AudioSource AS;
+
+	private bool canClick = false;
+
 	public void InputClick(int i) {
-		switch (i) {
-			case 0:
-				AudioSource.PlayClipAtPoint(RightClick, gameObject.transform.position);
-				gameObject.SendMessage("Win");
-				break;
-			case 1:
-				AudioSource.PlayClipAtPoint(WrongClick, gameObject.transform.position);
-				Hint.SetActive(true);
-				break;
-			case 2:
-				AudioSource.PlayClipAtPoint(WrongClick, gameObject.transform.position);
-				Hint.SetActive(true);
-				break;
-		}
+		if (canClick)
+			switch (i) {
+				case 0:
+					AudioSource.PlayClipAtPoint(RightClick, gameObject.transform.position);
+					gameObject.SendMessage("Win");
+					break;
+				case 1:
+					AudioSource.PlayClipAtPoint(WrongClick, gameObject.transform.position);
+					Hint.SetActive(true);
+					break;
+				case 2:
+					AudioSource.PlayClipAtPoint(WrongClick, gameObject.transform.position);
+					Hint.SetActive(true);
+					break;
+			}
 	}
 	public void FinishStory() {
 		StartCoroutine(Question());
 	}
 	IEnumerator Question() {
+		AS.volume = 0f;
 		yield return new WaitForSeconds(0.5f);
 		for (int i = 0; i < AnimalVoice.Length; i++) {
 			AnimalObj[i].GetComponent<Image>().sprite = AnimalSprite[i * 2 + 1];
@@ -39,6 +45,8 @@ public class Elephant : MonoBehaviour {
 		}
 		yield return new WaitForSeconds(0.5f);
 		AudioSource.PlayClipAtPoint(QuestionVoice, gameObject.transform.position);
-		yield return new WaitForSeconds(QuestionVoice.length);
+		yield return new WaitForSeconds(QuestionVoice.length + 0.5f);
+		AS.volume = 1f;
+		canClick = true;
 	}
 }
--- Assets/Scripts/Mission/Tiger.cs
+++ Assets/Scripts/Mission/Tiger.cs
@@ -5,31 +5,38 @@ using UnityEngine.UI;
 public class Tiger : MonoBehaviour {
 	public GameObject PlayPanel;
 	public AudioClip QestionVoice;
-	public AudioClip Wrong,Correct;
+	public AudioClip Wrong, Correct;
 	public GameObject TigetObj;
 	public Sprite TigerSprite;
 	public GameObject SoundIcon;
 	public Sprite SoundIconSprite;
 	public GameObject HintChoice;
+	public AudioSource AS;
+
+	private bool canClick = false;
 
 	public void InputClick(int i) {
-		if (i == 0) {
-			PlayPanel.SetActive(false);
-			AudioSource.PlayClipAtPoint(Correct,gameObject.transform.position);
-			TigetObj.GetComponent<Image>().sprite = TigerSprite;
-			gameObject.SendMessage("Win");
-		} else {
-			AudioSource.PlayClipAtPoint(Wrong, gameObject.transform.position);
-			HintChoice.SetActive(true);
-		}
+		if (canClick)
+			if (i == 0) {
+				PlayPanel.SetActive(false);
+				AudioSource.PlayClipAtPoint(Correct, gameObject.transform.position);
+				TigetObj.GetComponent<Image>().sprite = TigerSprite;
+				gameObject.SendMessage("Win");
+			} else {
+				AudioSource.PlayClipAtPoint(Wrong, gameObject.transform.position);
+				HintChoice.SetActive(true);
+			}
 	}
 	public void FinishStory() {
 		StartCoroutine(Question());
 	}
 	IEnumerator Question() {
+		AS.volume = 0f;
 		PlayPanel.SetActive(true);
 		AudioSource.PlayClipAtPoint(QestionVoice, gameObject.transform.position);
 		yield return new WaitForSeconds(QestionVoice.length);
+		canClick = true;
+		AS.volume = 1f;
 		SoundIcon.GetComponent<Image>().sprite = SoundIconSprite;
 	}
 }
